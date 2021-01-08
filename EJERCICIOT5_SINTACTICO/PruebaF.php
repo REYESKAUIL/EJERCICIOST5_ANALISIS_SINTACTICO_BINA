892ec9aa764df02160ba87daee4717e17672f3fb
REYESKAUIL <73450709+REYESKAUIL@users.noreply.github.com>
2021-01-07 18:19:20 -0600

    Add files via upload

---
+++ EJERCICIOT5_SINTACTICO/PruebaF.php
@@ -0,0 +1,29 @@
+<?php
+$valorD = 8.4;
+$valorE = 10;
+$ValorEx = 20;
+
+$valorD + $ValorEx;
+$suma = $valorD * 2;
+$suma;
+16 + 8 * 21;
+9 * 20 + 3;
+31 +2 - 10;
+
+$ope = 14 * ( 15 - 13 );
+$ope;
+$factible = $suma;
+
+if( $valorD <= $valorE){  $a; }
+else{ $a; }
+if( $valorD = $valorE){  $a; }
+if( $valorD != $valorE){  $a; }
+
+for($i = 1; $i < $c; $i++) { $i; }
+
+while($i <= $valorD){ $i++; }
+
+$a = 0;
+do{ $a; } while( $a < 0 );
+
+?>
